be4a401afa71cfbc4d60050b5dc56738e2474f1c
Duy <tominhduy09@gmail.com>
2024-02-24 14:22:28 +0200

    fix typo in MQTT payload

--- esp32/esp32_sketch/esp32_sketch.ino
+++ esp32/esp32_sketch/esp32_sketch.ino
@@ -1,33 +1,31 @@
-#include <NTPClient.h>
-
-#include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
-#include <WiFi.h>
-#include <Wire.h>
+#include <Adafruit_Sensor.h>
 #include <NTPClient.h>
-#include <WiFiUdp.h>
 #include <PubSubClient.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <Wire.h>
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 // CHANGE PARAMETERS!
 // Setup WiFi connection
-const char* ssid = ""; // CHANGE THIS!
+const char* ssid = "";      // CHANGE THIS!
 const char* password = "";  // CHANGE THIS!
 
 // Setup MQTT connection
-const char* mqtt_server = ""; // CHANGE THIS!
-const char* mqtt_username = ""; // CHANGE THIS!
-const char* mqtt_password = ""; // CHANGE THIS!
-const char* clientID = "Home"; // CHANGE THIS!
+const char* mqtt_server = "";    // CHANGE THIS!
+const char* mqtt_username = "";  // CHANGE THIS!
+const char* mqtt_password = "";  // CHANGE THIS!
+const char* clientID = "Home";   // CHANGE THIS!
 const char* readings_topic = "readings";
 char data[80];
 
 // Set timezone in GMT time, i.e: timeZone = 2 for GMT +2
-int timeZone = 2; // CHANGE THIS! 
+int timeZone = 2;  // CHANGE THIS!
 
 // Setup I2C sensor connection
-Adafruit_BMP280 bmp; 
+Adafruit_BMP280 bmp;
 
 // Initialise WiFi and MQTT client object
 WiFiClient wifiClient;
@@ -35,7 +33,8 @@ PubSubClient client(mqtt_server, 1883, wifiClient);
 
 // Initialise time client object
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, timeZone * 3600); // Setup an UDP Client to retrieve date and time
+NTPClient timeClient(
+    ntpUDP, timeZone * 3600);  // Setup an UDP Client to retrieve date and time
 String formattedDate;
 String date;
 String timestamp;
@@ -45,12 +44,11 @@ unsigned long delayTime = 60000;
 
 void setup() {
   Serial.begin(115200);
-  initWiFi(); // Connect to WiFi
-  initSensor(); // Setup sensor
+  initWiFi();    // Connect to WiFi
+  initSensor();  // Setup sensor
 }
 
-
-void loop() { 
+void loop() {
   connect_MQTT();
   Serial.setTimeout(2000);
   send_MQTT();
@@ -69,16 +67,17 @@ void send_MQTT() {
   Serial.print(temperature);
   Serial.println(" *C");
 
-  String readings = "{\"temperature\":\"" + String(temperature) + "\", \"date\":\"" + date + "\", \"time\":\"" + timestamp + "\", \"location\":\"" + clientID "\"}";
+  String readings = "{\"temperature\":\"" + String(temperature) +
+                    "\", \"date\":\"" + date + "\", \"time\":\"" + timestamp +
+                    "\", \"location\":\"" + clientID + "\"}";
   readings.toCharArray(data, (readings.length() + 1));
 
-  if(client.publish(readings_topic, data)) {
+  if (client.publish(readings_topic, data)) {
     Serial.println("Readings sent!");
-  }
-  else{
+  } else {
     Serial.println("Readings failed to send!");
   }
-  
+
   Serial.println();
 }
 
@@ -86,7 +85,7 @@ void initWiFi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.print(("Connecting to WiFi ..."));
-  while (WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
     delay(1000);
   }
@@ -96,14 +95,12 @@ void initWiFi() {
 }
 
 void connect_MQTT() {
-
   // Connect to MQTT Broker
-    while (!client.connected()) {
+  while (!client.connected()) {
     Serial.println("Connecting to MQTT broker ...");
     if (client.connect(clientID, mqtt_username, mqtt_password)) {
       Serial.println("MQTT broker connected!");
-    } 
-    else {
+    } else {
       Serial.print("MQTT broker not connected: ");
       Serial.println(client.state());
       delay(5000);
@@ -116,10 +113,11 @@ void initSensor() {
 
   bool status;
 
-  status = bmp.begin(0x76);  
+  status = bmp.begin(0x76);
   if (!status) {
     Serial.println("Could not find a valid BMP280 sensor, check wiring!");
-    while (1);
+    while (1)
+      ;
   }
 
   Serial.println("-- Default Test --");
@@ -127,16 +125,17 @@ void initSensor() {
 
   Serial.println();
 
-  timeClient.begin(); // Start timeClient
+  timeClient.begin();  // Start timeClient
 }
 
 void getAndFormatTime() {
-  while(!timeClient.update()){
+  while (!timeClient.update()) {
     timeClient.forceUpdate();
   }
   formattedDate = timeClient.getFormattedDate();
 
   int splitTime = formattedDate.indexOf("T");
   date = formattedDate.substring(0, splitTime);
-  timestamp = formattedDate.substring(splitTime + 1, formattedDate.length() - 1);
+  timestamp =
+      formattedDate.substring(splitTime + 1, formattedDate.length() - 1);
 }
